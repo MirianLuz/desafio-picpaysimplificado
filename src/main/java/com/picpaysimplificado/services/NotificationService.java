8ead53c9990a511d6f6cddf84b028261a1a71ec7
MirianLuz <61255081+MirianLuz@users.noreply.github.com>
2024-03-11 15:42:27 -0300

    feat: create TransactionController

--- src/main/java/com/picpaysimplificado/services/NotificationService.java
+++ src/main/java/com/picpaysimplificado/services/NotificationService.java
@@ -18,14 +18,13 @@ public class NotificationService {
         String email = user.getEmail();
         NotificationDTO notificationRequest = new NotificationDTO(email, message);
 
-        ResponseEntity<String> notificationResponse =restTemplate.postForEntity("https://run.mocky.io/v3/54dc2cf1-3add-45b5-b5a9-6bf7e7f1f4a6", notificationRequest, String.class);
-
-        if(!(notificationResponse.getStatusCode() == HttpStatus.OK)){
-            System.out.println("erro ao enviar notificacao");
-            throw new Exception("Servico de notificacao esta fora do ar");
-
-
-        }
+//        ResponseEntity<String> notificationResponse =restTemplate.postForEntity("https://run.mocky.io/v3/54dc2cf1-3add-45b5-b5a9-6bf7e7f1f4a6", notificationRequest, String.class);
+//
+//        if(!(notificationResponse.getStatusCode() == HttpStatus.OK)){
+//            System.out.println("erro ao enviar notificacao");
+//            throw new Exception("Servico de notificacao esta fora do ar");
+//
+//        }
 
     }
 }
--- src/main/java/com/picpaysimplificado/services/TransactionService.java
+++ src/main/java/com/picpaysimplificado/services/TransactionService.java
@@ -26,7 +26,10 @@ public class TransactionService {
     @Autowired
     private RestTemplate restTemplate;
 
-    public void createTransaction(TransactionDTO transactionDTO) throws Exception {
+    @Autowired
+    private NotificationService notificationService;
+
+    public Transaction createTransaction(TransactionDTO transactionDTO) throws Exception {
         User sender = this.userService.findUserById((transactionDTO.senderId()));
         User receiver = this.userService.findUserById(transactionDTO.receiverId());
 
@@ -49,6 +52,11 @@ public class TransactionService {
         this.transactionRepository.save(newTransaction);
         this.userService.saveUser(sender);
         this.userService.saveUser(receiver);
+
+        this.notificationService.sendNotification(sender, "Transacao realizada com sucesso");
+        this.notificationService.sendNotification(receiver, "Transacao recebida com sucesso");
+
+        return newTransaction;
     }
 
     public boolean authorizeTransaction(User sender, BigDecimal value){
